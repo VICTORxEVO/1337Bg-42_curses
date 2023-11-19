10b951352bd56d2f58376fbf9b9c514423e0b6e9
VICTORxEVO <yassirsbai04@gmail.com>
2023-11-19 18:48:56 +0100

    1337

--- libft.a/ft_strtrim.c
+++ libft.a/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ysbai-jo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 18:06:51 by ysbai-jo          #+#    #+#             */
-/*   Updated: 2023/11/19 18:33:10 by ysbai-jo         ###   ########.fr       */
+/*   Updated: 2023/11/19 18:48:35 by ysbai-jo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	start = 0;
 	while (s1[start] && check(s1[start], set))
 		++start;
-	while (s1[end] && check(s1[end]))
+	while (s1[end] && check(s1[end], set))
 		++end;
 	if (end < 0)
 		return (ft_substr(s1, start, 0));
