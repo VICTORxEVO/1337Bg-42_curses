d148127a1d4178835015138d968bc22c8cd75051
VICTORxEVO <yassirsbai04@gmail.com>
2023-11-22 17:23:31 +0100

    V1.09

--- libft.a/ft_lstnew_bonus.c
+++ libft.a/ft_lstnew_bonus.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysbai-jo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 17:22:56 by ysbai-jo          #+#    #+#             */
+/*   Updated: 2023/11/22 17:22:57 by ysbai-jo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-t_list  *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-    t_list *ptr;
+	t_list	*ptr;
 
-    ptr = malloc (sizeof(t_list));
-    if (!ptr)
-        return (NULL);
-    ptr->content = content;
-    ptr->next = NULL;
-    return (ptr);
+	ptr = malloc(sizeof(t_list));
+	if (!ptr)
+		return (NULL);
+	ptr->content = content;
+	ptr->next = NULL;
+	return (ptr);
 }
