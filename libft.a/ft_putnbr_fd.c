b369fe218e5858dfbd48f851eacee8a6843d4826
VICTORxEVO <yassirsbai04@gmail.com>
2023-11-22 14:08:07 +0100

    v1.1

---
+++ libft.a/ft_putnbr_fd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysbai-jo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 14:04:29 by ysbai-jo          #+#    #+#             */
+/*   Updated: 2023/11/22 14:04:30 by ysbai-jo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	n_pro;
+
+	n_pro = n;
+	if (n < 0)
+	{
+		n_pro *= -1;
+		ft_putchar_fd('-', fd);
+	}
+	else if (n >= 0 && n <= 9)
+		ft_putchar_fd(n + '0', fd);
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd((n_pro % 10 + '0'), fd);
+	}
+}
