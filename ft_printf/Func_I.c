ec3a265a865389dfd29c7204e52753fe0a27d808
VICTORxEVO <yassirsbai04@gmail.com>
2023-12-09 16:23:56 +0100

    V1.002 + PROTETION I

--- ft_printf/Func_I.c
+++ ft_printf/Func_I.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Func_I                                             :+:      :+:    :+:   */
+/*   Func_I.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ysbai-jo <ysbai-jo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 16:29:55 by ysbai-jo          #+#    #+#             */
-/*   Updated: 2023/12/06 16:11:57 by ysbai-jo         ###   ########.fr       */
+/*   Updated: 2023/12/09 16:22:02 by ysbai-jo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_putchar(char c, int *len)
 	(*len)++;
 }
 
-void ft_putstr(const char *s, int *len)
+void	ft_putstr(const char *s, int *len)
 {
 	if (!s)
 	{
@@ -34,21 +34,24 @@ void ft_putstr(const char *s, int *len)
 
 void	ft_putnbr(int n, int *len)
 {
-	unsigned int n_pro;
+	unsigned int	n_pro;
 
 	n_pro = n;
 	if (n < 0)
-		ft_putchar('-');
+	{
+		ft_putchar('-', len);
+		n_pro *= -1;
+	}
 	if (n_pro < 10)
 		ft_putchar(n_pro + '0', len);
 	else
 	{
 		ft_putnbr(n_pro / 10, len);
-		ft_putchar(n_pro % 10, len);
+		ft_putchar(n_pro % 10 + '0', len);
 	}
 }
 
-void ft_putnbrv2(unsigned int n, int *len)
+void	ft_putnbrv2(unsigned int n, int *len)
 {
 	if (n < 10)
 		ft_putchar(n + '0', len);
--- ft_printf/pf.h
+++ ft_printf/pf.h
@@ -6,15 +6,24 @@
 /*   By: ysbai-jo <ysbai-jo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 10:58:59 by ysbai-jo          #+#    #+#             */
-/*   Updated: 2023/12/06 11:55:46 by ysbai-jo         ###   ########.fr       */
+/*   Updated: 2023/12/09 16:21:41 by ysbai-jo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PF_H
-#define PF_H
+# define PF_H
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
+int		ft_printf(const char *s, ...);
+void	ft_putchar(char c, int *len);
+void	ft_putstr(const char *s, int *len);
+void	ft_putnbr(int n, int *len);
+void	ft_putnbrv2(unsigned int n, int *len);
+void	ft_putnbr_hec(unsigned int nb, char type, int *len);
+void	ft_putadrr(void *f, int *len);
+
+#endif
